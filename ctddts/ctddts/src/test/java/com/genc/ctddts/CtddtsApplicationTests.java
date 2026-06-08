5f7cd9e238b20193ce08bb125e9f527b254ae2ed
Soundararajan <2503723@cognizant.com>
2026-06-08 11:17:25 -0700

    Adding template for spring boot

---
+++ ctddts/ctddts/src/test/java/com/genc/ctddts/CtddtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genc.ctddts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtddtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
