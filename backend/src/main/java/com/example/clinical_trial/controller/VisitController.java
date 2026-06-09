46fd6be2b359dd8d758f1de02d295e31cd051ce3
S S <2503504@cognizant.com>
2026-06-09 08:13:54 -0700

    added backend folder to the main

---
+++ backend/src/main/java/com/example/clinical_trial/controller/VisitController.java
@@ -0,0 +1,32 @@
+package com.example.clinical_trial.controller;
+
+import com.example.clinical_trial.model.VisitRecord;
+import com.example.clinical_trial.service.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/visits")
+@CrossOrigin(origins = "http://localhost:63342")
+public class VisitController {
+    @Autowired
+    private VisitService visitService;
+
+    @PostMapping("/schedule")
+    public VisitRecord scheduleVisit(@RequestBody VisitRecord visit) {
+        return visitService.scheduleVisit(visit);
+    }
+
+    @GetMapping("/history/{subjectId}")
+    public List<VisitRecord> getVisitHistory(@PathVariable Long subjectId) {
+        return visitService.getVisitHistory(subjectId);
+    }
+
+    @PostMapping("/{visitId}/lock")
+    public VisitRecord lockCrf(@PathVariable Long visitId) {
+        return visitService.lockCrf(visitId);
+    }
+}
