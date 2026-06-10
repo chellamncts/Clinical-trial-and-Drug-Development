0de4cf5cd2df5bddcfb104eeec5c3d96eaecad72
S S <2503504@cognizant.com>
2026-06-09 22:47:05 -0700

    Add backend resources and test files

---
+++ backend/ctds/src/test/java/com/genc/ctds/CtdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genc.ctds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
